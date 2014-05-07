80b0f9367ddb3d313e4212f8bf85324157199943
Kallehz <kallehz@gmail.com>
2014-05-07 16:31:09 +0000

    Register og Login patch v1.1

--- PandaApp/PandaApp/Models/AccountViewModels.cs
+++ PandaApp/PandaApp/Models/AccountViewModels.cs
@@ -63,11 +63,11 @@ namespace PandaApp.Models
     public class UserViewModel
     {
         //Log in
-        //[Required]
+        [Required]
         [Display(Name = "User name")]
         public string UserName { get; set; }
 
-        //[Required]
+        [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
@@ -75,11 +75,11 @@ namespace PandaApp.Models
         [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
 
-        // Register
+        //Register
         [Display(Name = "Select your User name")]
         public string RegisterUserName { get; set; }
 
-        //[Required]
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
