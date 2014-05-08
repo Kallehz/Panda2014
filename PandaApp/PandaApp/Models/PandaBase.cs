cc20dd53fb0bd3440ac52864aa1670c5fbc476f9
thoddi <thorvardur13@ru.is>
2014-05-08 14:04:04 +0000

    Histories

--- PandaApp/PandaApp/Models/PandaBase.cs
+++ PandaApp/PandaApp/Models/PandaBase.cs
@@ -13,5 +13,6 @@ namespace PandaApp.Models
         public DbSet<Request> Requests { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Contributor> Contributors { get; set; }
+        public DbSet<History> Histories { get; set; }
     }
 }
