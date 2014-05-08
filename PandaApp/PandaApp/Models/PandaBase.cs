b372d2a7625b0796d4b2c6879e4a20eb66e1cb6f
thoddi <thorvardur13@ru.is>
2014-05-08 13:58:15 +0000

    Contributor

--- PandaApp/PandaApp/Models/PandaBase.cs
+++ PandaApp/PandaApp/Models/PandaBase.cs
@@ -12,5 +12,6 @@ namespace PandaApp.Models
         public DbSet<SubtitleLine> SubtitleLines { get; set; }
         public DbSet<Request> Requests { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contributor> Contributors { get; set; }
     }
 }
