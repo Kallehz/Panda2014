617d82869890f6c464b28070ef76b49006f57bb5
thoddi <thorvardur13@ru.is>
2014-05-08 14:44:05 +0000

    History aftur

---
+++ PandaApp/PandaApp/Models/History.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PandaApp.Models
+{
+    public class History
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public string Description { get; set; }
+        public int SubtitleId { get; set; }
+    }
+}
