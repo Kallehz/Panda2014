f83e06781e89b571a813e607045a6ea64e675bed
Alexander Björnsson <alexanderbjornsson@gmail.com>
2014-05-08 13:05:44 +0000

    Bætti við SubtitleLine klasa

---
+++ PandaApp/PandaApp/Models/SubtitleLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PandaApp.Models
+{
+    public class SubtitleLine
+    {
+        public int ID { get; set; }
+
+        public int subtitleID { get; set; }
+
+        public string text  { get; set; }
+
+        TimeSpan timeFrom { get; set; }
+
+        TimeSpan timeTo { get; set; }
+        
+    }
+}
