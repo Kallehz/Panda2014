e10cfc38641b1db9e5055017f90fbcfb2345891d
arniola <arniola@gmail.com>
2014-05-09 13:34:52 +0000

    Upload controller líka

---
+++ PandaApp/PandaApp/Controllers/UploadController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+
+namespace PandaApp.Controllers
+{
+    public class UploadController : Controller
+    {
+        //
+        // GET: /Upload/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Code from MS file upload tutorial http://support.microsoft.com/kb/323246
+
+        protected System.Web.UI.HtmlControls.HtmlInputFile File1;
+        protected System.Web.UI.HtmlControls.HtmlInputButton Submit1;
+
+        private void Submit1_ServerClick(object sender, System.EventArgs e)
+        {
+           
+            if ((File1.PostedFile != null) && (File1.PostedFile.ContentLength > 0))
+            {
+                string fn = System.IO.Path.GetFileName(File1.PostedFile.FileName);
+                string SaveLocation = Server.MapPath("Data") + "\\" + fn;
+                try
+                {
+                    File1.PostedFile.SaveAs(SaveLocation);
+                    Response.Write("The file has been uploaded.");
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Error: " + ex.Message);
+                    //Note: Exception.Message returns detailed message that describes the current exception. 
+                    //For security reasons, we do not recommend you return Exception.Message to end users in 
+                    //production environments. It would be better just to put a generic error message. 
+                }
+            }
+            else
+            {
+                Response.Write("Please select a file to upload.");
+            }
+        }
+
+        //End of code that might not belong here
+	}
+}
