feb5962259c22fe3dff372ae7ec696d9c3ce1f4a
egilsster <egilsster@gmail.com>
2014-05-10 11:47:37 +0000

    semi working search on index

--- PandaApp/PandaApp/Controllers/SubtitleController.cs
+++ PandaApp/PandaApp/Controllers/SubtitleController.cs
@@ -10,9 +10,30 @@ namespace PandaApp.Controllers
 {
     public class SubtitleController : Controller
     {
+        PandaRepo db = new PandaRepo();
+
         public ActionResult DisplayEditPage(int subtitleID)
         {
             return View(new EditViewModel(subtitleID));
+        }
+
+        public ActionResult SearchResult(string title, string language)
+        {
+            SubAndReq SandR = new SubAndReq();
+
+            SandR.Requests = (from item in db.GetAllRequests()
+                              where (item.Title == title) && 
+                              (item.Language == language)
+                              orderby item.Upvotes descending
+                              select item).Take(15);
+
+            SandR.Subtitles = (from item in db.GetAllSubtitles()
+                               where (item.Title == title) && 
+                               (item.Language == language)
+                               orderby item.DateCreated descending
+                               select item).Take(15);
+
+            return View(SandR);
         }
 	}
 
