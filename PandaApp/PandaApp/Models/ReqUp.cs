e55bb21c18294811d33abfb5af29ceaaf9470230
thoddi <thorvardur13@ru.is>
2014-05-12 19:50:47 +0000

    upvote

---
+++ PandaApp/PandaApp/Models/ReqUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PandaApp.Models
+{
+    public class ReqUp
+    {
+        public Request request { get; set; }
+        public bool upvoted { get; set; }
+    }
+}
