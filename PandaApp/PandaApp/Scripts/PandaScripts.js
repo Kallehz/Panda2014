2592a41562617541afd00a7171d331aa0361faef
alli93 <alexanderb13@ru.is>
2014-05-12 20:27:02 +0000

    Færði ajax scriptu í Scripts/Pandascripts og pússaði markupið á edit síðunni

---
+++ PandaApp/PandaApp/Scripts/PandaScripts.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('form').submit(function (e) {
+        var text = $("#" + this.id + " #lineText").val();
+        var id = $("#" + this.id + " #lineID").val();
+        $.ajax({
+            type: 'post',
+            url: '/Subtitle/UpdateSubtitleLine',
+            data: { id: id, text: text },
+        });
+        e.preventDefault();
+    });
+});
