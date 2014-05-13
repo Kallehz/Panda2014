58f2c0fe3d8de2a2b98babc7d656b2968b7e6c1d
Alexander Björnsson <alexanderbjornsson@gmail.com>
2014-05-13 17:35:23 +0000

    Basic alla-sync (sink?)

--- PandaApp/PandaApp/Scripts/EditScript.js
+++ PandaApp/PandaApp/Scripts/EditScript.js
@@ -4,11 +4,25 @@
         var id = $("#" + this.id + " #lineID").val();
         var timeStart = $("#" + this.id + " #lineStart").val();
         var timeStop = $("#" + this.id + " #lineStop").val();
-        $.ajax({
-            type: 'post',
-            url: '/Subtitle/UpdateSubtitleLine',
-            data: { id: id, text: text, timeStart: timeStart, timeStop: timeStop },
-        });
+        if (text == "") {
+            $("#edit_msg").html("Text field must not be empty");
+            $("#edit_msg").fadeIn(600);
+            $("#edit_msg").css({ display: "inline-table", color: "#eb6864" });
+        }
+        else {
+            $.ajax({
+                type: 'post',
+                url: '/Subtitle/UpdateSubtitleLine',
+                data: { id: id, text: text, timeStart: timeStart, timeStop: timeStop },
+                success: function () {
+                    $("#edit_msg").css({ display: "none" });
+                    $("#edit_msg").html("Your changes have been saved");
+                    $("#edit_msg").fadeIn(600);
+                    $("#edit_msg").css({ display: "inline-table", color: "lightgreen" });
+                    $("#edit_msg").fadeOut(2000);
+                }
+            });
+        }
         e.preventDefault();
     });
 });
