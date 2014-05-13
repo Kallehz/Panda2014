9a501d8016eaf647524af3a7cc06115d300437c5
alli93 <alexanderb13@ru.is>
2014-05-13 21:42:05 +0000

    Css selectiveevolution

--- PandaApp/PandaApp/Scripts/EditScript.js
+++ PandaApp/PandaApp/Scripts/EditScript.js
@@ -1,13 +1,14 @@
 ﻿$(document).ready(function () {
     $('form').submit(function (e) {
-        var text = $("#" + this.id + " #lineText").val();
-        var id = $("#" + this.id + " #lineID").val();
-        var timeStart = $("#" + this.id + " #lineStart").val();
-        var timeStop = $("#" + this.id + " #lineStop").val();
+        var selectorPrefix = "#" + this.id;
+        var text = $(selectorPrefix + " #lineText").val();
+        var id = $(selectorPrefix + " #lineID").val();
+        var timeStart = $(selectorPrefix + " #lineStart").val();
+        var timeStop = $(selectorPrefix + " #lineStop").val();
         if (text == "") {
-            $("#edit_msg").html("Text field must not be empty");
-            $("#edit_msg").fadeIn(600);
-            $("#edit_msg").css({ display: "inline-table", color: "#eb6864" });
+            $(selectorPrefix + " div[id=\"submit_div\"] #edit_msg").html("Text field must not be empty");
+            $(selectorPrefix + " div[id=\"submit_div\"] #edit_msg").fadeIn(600);
+            $(selectorPrefix + " div[id=\"submit_div\"] #edit_msg").css({ display: "inline-table", color: "#eb6864" });
         }
         else {
             $.ajax({
@@ -15,12 +16,12 @@
                 url: '/Subtitle/UpdateSubtitleLine',
                 data: { id: id, text: text, timeStart: timeStart, timeStop: timeStop },
                 success: function () {
-                    alert(("input[value=\"" + id + "\"]"));
-                    $("input[value=\"" + id + "\"] ~ div[id=\"submit_div\"] #edit_msg").css({ display: "none" });
-                    $("input[value=\"" + id + "\"] ~ div[id=\"submit_div\"] #edit_msg").html("Your changes have been saved");
-                    $("input[value=\"" + id + "\"] ~ div[id=\"submit_div\"] #edit_msg").fadeIn(600);
-                    $("input[value=\"" + id + "\"] ~ div[id=\"submit_div\"] #edit_msg").css({ display: "inline-table", color: "lightgreen" });
-                    $("input[value=\"" + id + "\"] ~ div[id=\"submit_div\"] #edit_msg").fadeOut(2000);
+                    var selector = "input[value=\"" + id + "\"] ~ div[id=\"submit_div\"] #edit_msg";
+                    $(selector).css({ display: "none" });
+                    $(selector).html("Your changes have been saved");
+                    $(selector).fadeIn(600);
+                    $(selector).css({ display: "inline-table", color: "lightgreen" });
+                    $(selector).fadeOut(2000);
                 }
             });
         }
