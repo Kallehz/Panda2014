90124aa580926baf9c582724a074eaab009009cb
Alexander Björnsson <alexanderbjornsson@gmail.com>
2014-05-13 12:46:45 +0000

    Tiltekt

--- PandaApp/PandaApp/Models/EditViewModel.cs
+++ PandaApp/PandaApp/Models/EditViewModel.cs
@@ -11,9 +11,6 @@ namespace PandaApp.Models
         public string Title { get; set; }
         public string Language { get; set; }
         public IEnumerable<SubtitleLine> Lines { get; set; }
-        public string TimeFrom { get; set; }
-        public string TImeTO { get; set; }
-
         public EditViewModel(int subtitleID)
         {
             SubtitleID = subtitleID;
