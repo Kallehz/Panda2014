867c4065d092fe8b79fa281dcb99b2d334bd0ce5
egilsster <egilsster@gmail.com>
2014-05-15 15:11:04 +0000

    nada

--- PandaApp/PandaApp/Controllers/RequestController.cs
+++ PandaApp/PandaApp/Controllers/RequestController.cs
@@ -135,9 +135,7 @@ namespace PandaApp.Controllers
         public void Upvote(int id)
         {
             PandaBase panda = new PandaBase();
-            /*ReqUp requp = new ReqUp();
-            requp.request = db.GetRequestById(id);*/
-
+            
             if(db.GetReqUpBool(id, db.GetUserByName(User.Identity.Name).ID))
             {
                 Request req = panda.Requests.Single(re => re.ID == id);
@@ -147,8 +145,6 @@ namespace PandaApp.Controllers
                 panda.Upvoters.Add(upvoter);
 
                 panda.SaveChanges();
-
-                //requp.upvoted = false;
             }
         }
 
