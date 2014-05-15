785e7a000b6564f6559b2401ad6b0c95e5b01245
alli93 <alexanderb13@ru.is>
2014-05-15 00:51:38 +0000

    Black magic update script yoloswag

--- PandaApp/PandaApp/Scripts/UpvoteScript.js
+++ PandaApp/PandaApp/Scripts/UpvoteScript.js
@@ -1,14 +1,15 @@
 ﻿$(document).ready(function () {
-    $('form[id=\"upvote\"]').submit(function (e) {
-        e.preventDefault();
-        var id = $("input[name=\"id\"]").val();
+    $('form[id=\"upvote\"] button[type=\"submit\"]').click(function (e) {
+        var upvotes = parseInt($(this).html()[37]);
+        $(this).html(upvotes + 1 + " <span class=\"glyphicon glyphicon-arrow-up\"></span>");
+        $(this).children().css({ "color": "lightgreen" });
+        $(this).children().fadeOut(1000);
+        var id = $(this).siblings("input").val();
         $.ajax({
             type: 'post',
             url: '/Request/Upvote',
             data: { id: id },
         });
-        $(this).html("Upvoted!");
-        $(this).css({ color: "lightgreen" })
-        $(this).fadeOut(1000);
+        e.preventDefault();
     });
 });
