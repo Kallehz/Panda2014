20fdb67cc5d693ef042fe6baa92646ab49535bf8
Alexander Björnsson <alexanderbjornsson@gmail.com>
2014-05-16 13:51:00 +0000

    þlih

--- PandaApp/PandaApp/Controllers/SubtitleController.cs
+++ PandaApp/PandaApp/Controllers/SubtitleController.cs
@@ -48,11 +48,17 @@ namespace PandaApp.Controllers
         {
             using (var context = new PandaBase())
             {
+                // Locates a SubtitleLine in the database with
+                // a given id
                 SubtitleLine line = (context.SubtitleLines.Where(l => l.ID == id)
                                     .FirstOrDefault<SubtitleLine>());
+                // This adds two line breaks at the end of the text,
+                // this is necessary for the .srt standard
                 line.Text = text + "\r\n" + "\r\n";
                 line.TimeFrom = timeStart;
                 line.TimeTo = timeStop;
+                // Marks the entry 'line' in the database as modified,
+                // this way code-first knows it should update this entry in the database
                 context.Entry(line).State = System.Data.Entity.EntityState.Modified;
                 context.SaveChanges();
             }
