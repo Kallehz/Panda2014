0a64e99aceb4d10093780895de635cbcbcccaf26
arniola <arniola@gmail.com>
2014-05-16 13:43:10 +0000

    Kommenti frá gaurnum hent út

--- PandaApp/PandaApp/Models/PandaBase.cs
+++ PandaApp/PandaApp/Models/PandaBase.cs
@@ -17,16 +17,5 @@ namespace PandaApp.Models
         public DbSet<Language> Languages { get; set; }
         public DbSet<Upvoter> Upvoters { get; set; }
 
-        /*
-         * protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            // ENTITY FRAMEWORK FLUENT API
-            modelBuilder.Entity<Comment>()
-                .HasOptional(p => p.Subtitle)
-                .WithMany(p => p.Comments)
-                .HasForeignKey(p => p.SubtitleId);
-
-            base.OnModelCreating(modelBuilder);
-        }*/
     }
 }
